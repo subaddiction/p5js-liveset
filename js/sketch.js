a1cdd45f836f5529c498a9b732fb49f55d953b09
mrk25 <mrk25@localhost.localdomain>
2016-05-23 08:11:27 +0200

    particles

--- js/sketch.js
+++ js/sketch.js
@@ -94,6 +94,8 @@ function loadPreset(preset, ring){
 
 
 function setup() {
+
+	frameRate(30);
 	createCanvas(windowWidth, windowHeight, WEBGL);
   	
   	for(i in rings){
@@ -105,6 +107,8 @@ function setup() {
 	
 	fft = new p5.FFT();
 	fft.setInput(mic);
+	
+	system = new ParticleSystem(createVector(0,0));
 	
 	
 	$(document).on({
@@ -128,6 +132,20 @@ function setup() {
 					loadPreset(0,1)
 					loadPreset(1,2)
 					loadPreset(2,0)
+				break;
+				
+				
+				
+				case 65: //a
+					system.injectParticles(1500);
+				break;
+				
+				case 83: //s
+					system.injectParticles(1500);
+				break;
+				
+				case 68: //d
+					system.injectParticles(1500);
 				break;
 			}
 		}
@@ -182,8 +200,23 @@ function draw(){
 	background(000);
 	ambientLight(128, 128, 128);
 	pointLight(255, 255, 255, 0, 0, 0);
-
+	
+	//return;
 	//pointLight(255, 255, 255, windowWidth/2, windowHeight/2, -10);
+	
+	push()
+	//rotateZ(frameCount);
+	rotX = rotX+(fft.getEnergy('bass')/5000);
+	rotateX(rotX);
+	
+	rotY = rotY+(fft.getEnergy('lowMid')/5000);
+	rotateY(rotY);
+	
+	if(fft.getEnergy('bass') >= 127){
+		system.injectParticles(5);
+	}
+  	system.run();
+	pop()
 	
 	for(i in rings){
 		
@@ -203,10 +236,101 @@ function draw(){
 		
 		ambientMaterial(fft.getEnergy(rings[i].r),fft.getEnergy(rings[i].g),fft.getEnergy(rings[i].b));
 		
+		//specularMaterial(fft.getEnergy(rings[i].r),fft.getEnergy(rings[i].g),fft.getEnergy(rings[i].b));
+		
+		
 		model(rings[i].model);
 		
 		pop();
 
 	}
+	
+	
+	
+	
+	
   
+}
+
+
+
+
+
+
+
+
+
+
+//var particlesNumber = 1000;
+//var pAccX = 1;
+//var pAccY = 1;
+//var pAccZ = 1;
+
+
+// A simple Particle class
+var Particle = function(position) {
+  //this.acceleration = createVector(random(-0.1, 0.1), random(-0.1, 0.1), random(-0.1, 0.1));
+  this.acceleration = createVector(random(-0.1, 0.1), 0, random(-0.1, 0.1));
+  //this.acceleration = createVector(1,1,1);
+  this.velocity = createVector(random(-0.1, 0.1), random(-0.1, 0.1), random(-0.1, 0.1));
+  //this.velocity = createVector(0, 0);
+  //this.position = position.copy();
+  this.position = createVector(0, 0);
+  this.lifespan = 255.0;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){  
+  //noise = createVector(random(-width, width), random(-width, width));
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  //this.position.add([random(-1,1), random(-1,1)]);
+  //this.position = noise;
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  translate(this.position.x, this.position.y, this.position.z);
+  box(10,10,10);
+  //plane(10,10)
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  if (this.lifespan < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length-1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
+
+ParticleSystem.prototype.injectParticles =  function(n){
+	for(i=0; i<=n; i++){
+		this.addParticle();
+	}
 }
